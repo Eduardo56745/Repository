a54195b15b65ff8103bd3480acce457f7ae62895
Eduardo Ortega <lalox1697@gmail.com>
2024-11-12 15:17:02 -0600

    Correcciones

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header('Analisis de datos de venta de choches')
+st.header('Análisis de datos de venta de coches')
 # Crear un botón para el histograma
 hist_button = st.button('Construir un histograma')
 # Crear un botón para el diagrama de dispersión
@@ -13,14 +13,14 @@ scatter_button = st.button('Construir un diagrama de dispersión')
 if hist_button:
     # mostrar el mensaje
     st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches.')
 
     fig = px.histogram(car_data, x='odometer')  # crear un histograma
 
     st.plotly_chart(fig, use_container_width=True)  # gráfico interactivo
 
 if scatter_button:
-    st.write('Creación de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches.')
 
     fig = px.scatter(car_data, x='odometer', y='price')
 
